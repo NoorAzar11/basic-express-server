bce417ebe34c6dc5213e2e07fabad744ccd5fb16
NoorAzar11 <nooraaz945@gamil.com>
2021-08-12 19:16:59 +0300

    updated

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,6 +1,6 @@
 'use strict';
-const server=require('../src/server');
 
+const server=require('../src/server');
 const supertest=require('supertest');
 const request=supertest(server.app);
 
@@ -8,23 +8,21 @@ const request=supertest(server.app);
 describe('will serve as your server ',()=>{
 
     it('handles not found requset 404 try again',async()=>{
-
-    
     const response=await request.get('/tryagain');
     expect(response.status).toEqual(404);
 });
 
 
+// it('handles not found requset try again 2',async()=>{
+//     const response=await request.get('/person');
+//     expect(response.status).toEqual(404);
+// });
+
 it('handles not found requset try again 2',async()=>{
-    const response=await request.get('/person');
+    const response=await request.post('/person');
     expect(response.status).toEqual(404);
 });
 
-it('handles internal errors try again',async()=>{
-    //add test cases
-    const response = await requset.post('/badreq');
-    expect(response.status).toEqual(500);
-});
 
 
 
--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,10 +1,12 @@
 'use strict'
 
 module.exports =(req,res,next)=>{
-    if(req.query.name){
-        next();
-    }
-    if(req.query.name==''){
-        next('invalid name');
-    }
+  const name=req.query.name;
+  if(typeof name=='string' && name.length!==0){
+      req.name=name;
+      next();
+
+  }else{
+      next('Error Has Been Found');
+  }
 };
